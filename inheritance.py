6af4581b7680761188811a1a229cdc9483bf086a
moni0015 <rmonika20200@gmail.com>
2025-09-30 10:22:49 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,34 @@
+#hybrid inheritance
+# class A:
+#     a=100
+#     b=200
+# class B(A):
+#     C=300
+#     D=400
+# class C(A):
+#     def __init__(self,e,f):
+#         self.e=e
+#         self.f=f
+# class D(B,C):
+#     pass
+# ob=D(1000,2000)
+# print(ob.a,ob.b,ob.C,ob.D,ob.e,ob.f)
+
+
+#POLY MORPHISM.
+# def add(a,b):
+#     print(a+b)
+
+# prev=add
+
+# def add(a,b,c,d):
+#     print(a+b+c+d)
+
+# prev(10,20)
+
+class Relation:
+    def __init__(self,a):
+        return self.a==other.a
+    def __eq__(self,a):
+        
+
